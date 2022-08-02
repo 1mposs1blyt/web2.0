84b896eb4f4a30a053c27de99ef49a801c485116
1mposs1blyt <alexandr.bryaginya@gmail.com>
2022-08-03 01:21:23 +0700

    +_+

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,68 +1,107 @@
 let data = [
     {
         "id": "1",
-        "author": "я",
-        "name": "названиие книги...",
-        "date": "02.08.2022"
+        "author": "книга 1",
+        "name": "названиие книги 1...",
+        "date": "01.08.2022"
     },
     {
         "id": "2",
-        "author": "я",
-        "name": "названиие книги...",
+        "author": "книга 2",
+        "name": "названиие книги 2...",
         "date": "02.08.2022"
     },
     {
         "id": "3",
-        "author": "я",
-        "name": "названиие книги...",
-        "date": "02.08.2022"
+        "author": "книга 3",
+        "name": "названиие книги 3...",
+        "date": "03.08.2022"
     },
     {
         "id": "4",
-        "author": "я",
-        "name": "названиие книги...",
-        "date": "02.08.2022"
+        "author": "книга 4",
+        "name": "названиие книги 4...",
+        "date": "04.08.2022"
     }
 ]
-console.log(data)
-function createBOOK(id, author, name, date) {
-    let findid = false;
+// console.log(data)
+function createBook(id, author, name, date) {
+    let findId = false; // флаг
     data.forEach(function (book) {
-        if (book.id = id) {
-            return true
+        if (book.id == id) {
+            findId = true
         }
     })
-    if (findid) {
-        return "Книга уже существует"
-    }else{
+    if (findId) {
+        return "Такая книга уже есть"
+    } else {
         data.push({
-            'id':id,
+            "id": id,
             "author": author,
-            "name":name,
+            "name": name,
             "date": date
         })
-        return "Книга добвлена"
+        return "Книга добавлена"
     }
 }
+function updateBOOK(id) {   
 
+}
+function DeleteBook(id, author, name, date) {
+
+}
+function ShowAllBooks(id, author, name, date){
+    let array = []
+    for (let i = 0; i < data.length; i++){
+        console.log(data[i])
+    }
+    
+}
+// РАБОТАЕТ НЕ ТРОГАТЬ!!!///////////////////////////////////////////////////////////////////////
 function searchbbbyid() {
-    let id = document.getElementById("1").innerHTML
-    let author = document.getElementById("2")
-    let name = document.getElementById("3")
-    let date = document.getElementById("4")
-    console.log(id)
-    data.forEach(function (book) {
-        if (book.id == id){
-            console.log(book.name)
+    let array = []
+
+    let id = document.getElementById("1")
+    console.log("По поиску:", id.value)
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i]["id"] == id.value) {
+            array.push(data[i]["author"], data[i]["name"], data[i]["date"], data[i]["id"])
+            console.log(array)
         }
-    })
+    }
 }
 function searchbbbyauthor() {
+    let array = []
 
+    let author = document.getElementById("2")
+    for (let i = 0; i < data.length; i++) {
+        if (data[i]["author"] == author.value) {
+            array.push(data[i]["author"], data[i]["name"], data[i]["date"], data[i]["id"])
+            console.log(array)
+        }
+    }
 }
 function searchbbbyname() {
+    let array = []
 
+    let name = document.getElementById("3")
+    for (let i = 0; i < data.length; i++) {
+        if (data[i]["name"] == name.value) {
+            array.push(data[i]["author"], data[i]["name"], data[i]["date"], data[i]["id"])
+            console.log(array)
+        }
+    }
 }
 function searchbbbydate() {
+    let array = []
 
+    let date = document.getElementById("4")
+    for (let i = 0; i < data.length; i++) {
+        if (data[i]["date"] == date.value) {
+            array.push(data[i]["author"], data[i]["name"], data[i]["date"], data[i]["id"])
+            console.log(array)
+        }
+    }
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////
